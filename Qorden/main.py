981fc1b598b5513230295c1a4b094b77d04bfc5e
Shahzaib Baig <sbaig3229@gmail.com>
2023-01-16 18:34:32 +0500

    updates

--- Qorden/main.py
+++ Qorden/main.py
@@ -2,9 +2,15 @@ from denoise import AudioDeNoise
 import Translator
 import TTS
 import assemblyai
+import os
+from gtts import gTTS
+import Real_stream
 
-audio=assemblyai.read_file('voice.wav')
+recording = Real_stream.recording()
+audio = assemblyai.read_file('output.wav')
 text=assemblyai.assembly(audio)
 trans_text = Translator.translation_class(text,'arabic')
 print(trans_text)
-TTS.STT(trans_text)
+tts = gTTS(trans_text, lang='ar', slow=False)
+tts.save('hello.mp3')
+os.remove('output.wav')
